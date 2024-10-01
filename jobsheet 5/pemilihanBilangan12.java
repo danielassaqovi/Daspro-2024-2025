dd94fe4b500ce014d447ef902739ca601aaeb0cb
damielassaqovi <palurejodaniel@gail.com>
2024-10-01 14:44:11 +0700

    bebas

---
+++ jobsheet 5/pemilihanBilangan12.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class pemilihanBilangan12{
+  public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+
+      System.out.print("Masukkan sebuah angka: ");
+      int angka = input.nextInt();
+
+      if (angka % 2 == 0)
+      {
+        System.out.println("Angka" + angka + " termasuk bilangan genap");
+      }
+      else
+      {
+        System.out.println("Angka" + angka + " termasuk bilangan ganjil");
+      }
+  }
+}
