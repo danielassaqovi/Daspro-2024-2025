9a5a7c56d68f105b8815c2bbce368d20926e7e9f
damielassaqovi <palurejodaniel@gail.com>
2024-10-09 21:15:13 +0700

    qris

--- jobsheet 6/pemilihan2percobaan2_12.java
+++ jobsheet 6/pemilihan2percobaan2_12.java
@@ -1,57 +1,96 @@
-
 import java.util.Scanner;
 
-public class pemilihan2percobaan2_12{
-  public static void main(String[] args) {
-      Scanner sc12 = new Scanner(System.in);
+public class pemilihan2percobaan2_12 {
+    
+    public static void main(String[] args) {
+        
+        // deklarasi scanner
+        Scanner input = new Scanner(System.in);
 
-      int pilihanMenu;
-      String member;
-      double diskon, totalBayar, harga;
+        // deklarasi nilai tetap
+        String member;
+        String pembayaran;
+        double harga = 0;
+        double diskon, total_harga, total_bayar;
 
-      System.out.println("=-----------------= ");
-      System.out.println("===MENU KAFE JTI===");
-      System.out.println("=-----------------= ");
-      System.out.println(" 1. kopi");
-      System.out.println(" 2. teh");
-      System.out.println(" 3. roti");
-      System.out.println("=--------------------------------------=");
-      System.out.print("Masukkan angka dari menu yang dipilih: ");
-      pilihanMenu = sc12.nextInt();
-      sc12.nextLine();
+        // judul
+        System.out.println("========================================");
+        System.out.println("=============== KAFE JTI ===============");
+        System.out.println("========================================");
 
-      System.out.print("Apakah punya member (y/n) ?: ");
-      member = sc12.nextLine();
-      System.out.println("=--------------------------------------=");
+        // daftar menu
+        System.out.println("Daftar menu : ");
+        System.out.println("1. Kopi");
+        System.out.println("2. Teh");
+        System.out.println("3. Roti");
 
-      if (member.equalsIgnoreCase("y")) {
-        diskon = 0.1;
-        System.out.println("Diskon 10%"); 
-      } else {
-        diskon = 0;
-        System.out.println("Tidak ada diskon");
-      }
+        System.out.println("========================================");
 
-      if (member.equalsIgnoreCase(member)) {
-          // diskon = 0.10;
-          // System.out.println("Besar diskon: 10% ");
-          if (pilihanMenu == 1) {
-            harga = 12000;
-            System.out.println("Harga kopi: " + harga);
-          }else if (pilihanMenu == 2){
-            harga = 7000;
-            System.out.println("Harga teh: " + harga);
-          }else if (pilihanMenu == 3){
-            harga = 2000;
-            System.out.println("Harga roti: " + harga);
-          }
-      }
+        // input pesanan
+        System.out.print("Masukkan angka menu yg akan dipesan\t : ");
+        int pilihan_menu = input.nextInt();
+        input.nextLine();
 
-      totalBayar = harga - (harga*diskon);
-      System.out.println("Total bayar " + totalBayar);
+        // input jumlah pesanan
+        System.out.print("Masukkan jumlah menu yg dipesan\t\t : ");
+        int jumlah = input.nextInt();
+        input.nextLine();
 
-      
+        // verifikasi member
+        System.out.print("Apakah memiliki member (y/n)\t\t : ");
+        member = input.nextLine();
 
+        // input jenis pembayaran
+        System.out.print("Metode pembayaran (QRIS/Cash)\t\t : ");
+        pembayaran = input.nextLine();
 
-  }
+        System.out.println("========================================");
+
+        // deklarasi harga menu
+        switch (pilihan_menu) {
+            case 1:
+                harga = 12000;
+                break;
+            case 2:
+                harga = 7000;
+                break;
+            case 3:
+                harga = 20000;
+                break;
+            default:
+                System.out.println("menu yang dipilih tidak valid");
+                return;
+        }
+
+        // hitung total harga
+        total_harga = harga * jumlah;
+
+        // logic jika member
+        if (member.equals("y")) {
+            diskon = 0.10; // diskon 10%
+            System.out.println("Besar diskon 10%");
+
+            // hitung total bayar setelah diskon
+            total_bayar = total_harga - (total_harga * diskon);
+        } 
+        else if (member.equals("n")) { 
+            // logic jika bukan member
+            total_bayar = total_harga;
+        } 
+        else {
+            System.out.println("Input member tidak valid");
+            return;
+        }
+
+        // potongan qris
+        if (pembayaran.equals("QRIS")) { 
+            total_bayar -= 1000;
+        }
+
+        // Output total harga
+        System.out.println("Harga normal : " + total_harga);
+        System.out.println("Total bayar : " + total_bayar);
+
+    }
+
 }
